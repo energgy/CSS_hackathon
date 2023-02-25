d42433eedac7603a4d715496e256d5e2568a41eb
Syzygy <zcemkap@ucl.ac.uk>
2023-02-25 17:32:25 +0000

    added some code for cog services API and requirements.txt

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-## github co pilot
+import azure
+import os
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
+
+# Set up Text Analytics client and authenticate with API key
+endpoint = "https://hackaton-css-serendipity.cognitiveservices.azure.com/"
+credential = AzureKeyCredential("6a1504350e484b63882efe45cfe1ac2a")
+text_analytics_client = TextAnalyticsClient(endpoint=endpoint, credential=credential)
+
+# Analyze sentiment of input text
+input_text = "I feel so sad and hopeless today."
+sentiment_analysis = text_analytics_client.analyze_sentiment([input_text])[0]
+
+# Print sentiment score and label
+print("Sentiment score: {}".format(sentiment_analysis.sentiment_scores))
+print("Sentiment label: {}".format(sentiment_analysis.sentiment))
+
+
